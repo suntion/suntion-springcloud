a56e67e87cc2b078cae2b163a1f71dc4c5c1d10f
scott <pai=3.1415926>
2019-07-11 14:47:00 +0800

    修改配置

--- suntion-hystrix-dashboard/src/main/java/com/suntion/HystrixDashboardApplication.java
+++ suntion-hystrix-dashboard/src/main/java/com/suntion/HystrixDashboardApplication.java
@@ -2,12 +2,8 @@ package com.suntion;
 
 import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.client.SpringCloudApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.netflix.turbine.EnableTurbine;
 import org.springframework.context.annotation.Bean;
@@ -18,9 +14,9 @@ import org.springframework.context.annotation.Bean;
 @SpringCloudApplication
 @EnableHystrixDashboard
 @EnableTurbine
-public class HystrixApplication {
+public class HystrixDashboardApplication {
     public static void main(String[] args) {
-        SpringApplication.run(HystrixApplication.class,args);
+        SpringApplication.run(HystrixDashboardApplication.class,args);
     }
 
 
